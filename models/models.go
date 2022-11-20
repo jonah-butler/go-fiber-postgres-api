7d8680a07ad8cfa08c8f6f6b589f16bb97caa0d8
Jonah Butler <jonahbutler6@gmail.com>
2022-11-20 10:44:09 -0500

    updated models to test out one to many relationship between users and shareable item

--- models/models.go
+++ models/models.go
@@ -15,26 +15,23 @@ type Location struct {
 
 type ShareableItem struct {
 	gorm.Model
-	ID           uint    `gorm:"primary key,autoIncrement" json:"id"`
-	Name         *string `gorm:"type:text" json:"name"`
-	Description  *string `gorm:"type:text" json:"description"`
-	Manufacturer *string `gorm:"type:text" json:"Manufacturer"`
-	ItemNumber   uint    `gorm:"-" json:"item_number"`
-	ModelNumber  uint    `gorm:"-" json:"model_number"`
-	User         User
-	UserID       int
+	ID     uint    `gorm:"primary key,autoIncrement" json:"id"`
+	Name   *string `gorm:"type:text" json:"name"`
+	User   *User   `gorm:"-"`
+	UserID uint    `json:"user_id"`
 }
 
 type User struct {
 	gorm.Model
-	ID        uint           `gorm:"primary key,autoIncrement" json:"id"`
-	Username  *string        `gorm:"unique" json:"username"`
-	Email     *string        `gorm:"unique" json:"email"`
-	Password  *string        `gorm:"type:varchar(500)" json:"password"`
-	Location  Location       `gorm:"-" json:"location"`
-	UpdatedAt time.Time      `gorm:"type:time" json:"updated_at"`
-	CreatedAt time.Time      `gorm:"type:time" json:"created_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	ID             uint            `gorm:"primary key,autoIncrement" json:"id"`
+	Username       *string         `gorm:"unique" json:"username"`
+	Email          *string         `gorm:"unique" json:"email"`
+	Password       *string         `gorm:"type:varchar(500)" json:"password"`
+	Location       Location        `gorm:"-" json:"location"`
+	UpdatedAt      time.Time       `gorm:"type:time" json:"updated_at"`
+	CreatedAt      time.Time       `gorm:"type:time" json:"created_at"`
+	DeletedAt      gorm.DeletedAt  `gorm:"index" json:"deleted_at"`
+	ShareableItems []ShareableItem `gorm:"ForeignKey:UserID"`
 }
 
 func MigrateTables(db *gorm.DB) error {
