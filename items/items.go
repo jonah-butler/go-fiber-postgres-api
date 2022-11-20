af2afa89f10b9463a8cd39ded828a9cd9c2f5104
Jonah Butler <jonahbutler6@gmail.com>
2022-11-20 10:45:10 -0500

    beginning stage of shareable items route

---
+++ items/items.go
@@ -0,0 +1,67 @@
+package items
+
+import (
+	"fmt"
+	"net/http"
+
+	database "go-postgres-fiber/connection"
+	models "go-postgres-fiber/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app fiber.Router) {
+	api := app.Group("/api")
+	item := api.Group("/item")
+
+	item.Post("/", CreateItem)
+	item.Get("/", GetItem)
+}
+
+func CreateItem(context *fiber.Ctx) error {
+	item := models.ShareableItem{}
+
+	err := context.BodyParser(&item)
+	if err != nil {
+		return context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{
+				"status":  400,
+				"message": "request failed",
+			},
+		)
+	}
+
+	err = database.Conn.Create(&item).Error
+	if err != nil {
+		fmt.Println("error creating item", err)
+		return context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create item"},
+		)
+	}
+
+	return context.Status(http.StatusOK).JSON(
+		&fiber.Map{
+			"status": http.StatusOK,
+			"user":   item,
+		},
+	)
+
+}
+
+func GetItem(context *fiber.Ctx) error {
+
+	item := models.ShareableItem{}
+
+	err := database.Conn.Find(&item).Error
+	if err != nil {
+		fmt.Println("could not lookup shareable items")
+		return context.Status(400).JSON(
+			&fiber.Map{"message": "could not lookup items"},
+		)
+	}
+
+	return context.Status(200).JSON(
+		&fiber.Map{"items": item},
+	)
+
+}
