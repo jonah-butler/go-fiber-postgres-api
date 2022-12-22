dfb9d08ce6df033f91f3f52d8d0be5da7d649acd
Jonah Butler <jonahbutler6@gmail.com>
2022-12-22 16:47:03 -0500

    added refresh token endpoint and grouped protected routes with a more dry middleware approach

--- users/users.go
+++ users/users.go
@@ -12,8 +12,15 @@ func SetupRoutes(app fiber.Router) {
 	api := app.Group("/api")
 	user := api.Group("/user")
 
+	// public user endpoints
 	user.Post("/register", CreateUser)
 	user.Post("/login", ValidateUser)
 	user.Get("/authenticate", AuthenticateUser)
-	user.Get("/private", SecureAuth(), PrivateUser)
+	user.Get("/refresh-access-token", RefreshAccessToken)
+
+	protected := user.Group("/p")
+	protected.Use(SecureAuth())
+
+	// private user endpoints
+	protected.Get("/private", PrivateUser)
 }
